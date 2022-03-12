53601560a7090aa486cacf55e4445c19711b9da2
Lukáš Zátopek <lukaza@centrum.cz>
2022-03-12 11:44:39 +0100

    Init repository

---
+++ VirtualRootTest/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualRootTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string GetInfo() => "I am returned info from server"; 
+    }
+}
